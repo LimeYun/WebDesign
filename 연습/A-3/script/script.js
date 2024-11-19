49c4ed5b8df852719e9155cb663a7c1978601eb0
LimeYun <jstar3183@naver.com>
2024-11-19 21:25:28 +0900

    modified:   "\354\227\260\354\212\265/A-3/css/style.css" 	modified:   "\354\227\260\354\212\265/A-3/index.html" 	modified:   "\354\227\260\354\212\265/A-3/script/script.js"

--- 연습/A-3/script/script.js
+++ 연습/A-3/script/script.js
@@ -1,8 +1,23 @@
-$(function() {
-    $(".main-menu").on("mouseover", function(){
+$(function(){
+
+    // 메뉴 마우스오버
+    $(".main-menu li").on("mouseover", function(){
         $(this).find(".sub-menu").stop().slideDown()
     })
-    $(".main-menu").on("mouseout", function(){
+    $(".main-menu li").on("mouseout", function(){
         $(this).find(".sub-menu").stop().slideUp()
     })
+
+    // 페이드 슬라이드
+    let index = 0
+    const speed = 500
+    const time = 3000
+    const $slide = $(".slide")
+    const count = $(".slide").length
+    $slide.not(":first").hide()
+    setInterval(function(){
+        index = (index + 1) % count
+        $slide.fadeOut(speed).hide()
+        $slide.eq(index).fadeIn(speed)
+    }, time)
 })
